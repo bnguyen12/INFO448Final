b4878facc6a385dbc57001a5273a66b72aaf14dc
Benny Nguyen <benny.nguyenx@gmail.com>
2018-06-01 11:18:37 -0700

    Made title, change background color, add fonts

--- app/src/main/java/edu/washington/bennyn/alarmlearning/MainActivity.kt
+++ app/src/main/java/edu/washington/bennyn/alarmlearning/MainActivity.kt
@@ -1,5 +1,8 @@
 package edu.washington.bennyn.alarmlearning
 
+import android.app.Activity
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
