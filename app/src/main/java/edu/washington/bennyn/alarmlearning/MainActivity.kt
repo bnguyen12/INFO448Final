bc7f33f600d2a5d11573a9bc825d98d37671c299
Benny Nguyen <benny.nguyenx@gmail.com>
2018-06-04 18:49:23 -0700

    Add transition into list of alarms

--- app/src/main/java/edu/washington/bennyn/alarmlearning/MainActivity.kt
+++ app/src/main/java/edu/washington/bennyn/alarmlearning/MainActivity.kt
@@ -1,15 +1,20 @@
 package edu.washington.bennyn.alarmlearning
 
-import android.app.Activity
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val launchBtn = findViewById<Button>(R.id.launchBtn)
+        launchBtn.setOnClickListener {
+            val intent = Intent(this, AlarmList::class.java)
+            startActivity(intent)
+        }
     }
 }
