9c7bc7faebd040bb9616a80a4394ece49c686f0f
Benny Nguyen <benny.nguyenx@gmail.com>
2018-06-05 18:19:59 -0700

    Add functionality to notification to track how many tasks I've done

--- app/src/main/java/edu/washington/bennyn/alarmlearning/AlarmReceiver.kt
+++ app/src/main/java/edu/washington/bennyn/alarmlearning/AlarmReceiver.kt
@@ -1,6 +1,7 @@
 package edu.washington.bennyn.alarmlearning
 
 import android.app.Notification
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -13,13 +14,45 @@ import com.orhanobut.hawk.Hawk
 class AlarmReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         Log.e("CheckIntent", "got it!")
-        val messsge = intent!!.getStringExtra("message")
+        val message = intent!!.getStringExtra("message")
+
+        val noButton = Intent(context, UpdateReceiver::class.java)
+        noButton.putExtra("answer", false)
+        val noIntent = PendingIntent.getBroadcast(context, 1, noButton, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val yesButton = Intent(context, UpdateReceiver::class.java)
+        yesButton.putExtra("answer", true)
+        val yesIntent = PendingIntent.getBroadcast(context, 0, yesButton, PendingIntent.FLAG_UPDATE_CURRENT)
+
         val mBuilder = NotificationCompat.Builder(context) //works on API 26 and lower
-                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setSmallIcon(R.drawable.clock)
                 .setContentTitle("AlarmLearning")
-                .setContentText(messsge)
+                .setContentText(message)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .addAction(R.drawable.alarm_off, "ignore", noIntent)
+                .addAction(R.drawable.alarm_on, "finished", yesIntent)
         val notificationMngr = NotificationManagerCompat.from(context!!)
         notificationMngr.notify(1, mBuilder.build())
+    }
+}
+
+// Updates whether or not the user did the action from the alarm
+class UpdateReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        NotificationManagerCompat.from(context!!).cancel(1) //cancel the notification after button press
+        val answer = intent!!.getBooleanExtra("answer", true)
+        Log.e("answerReceived", answer.toString())
+
+        Hawk.init(context).build()
+        val tasksDone = Hawk.get<Int>("tasksDone")
+        val tasksTotal = Hawk.get<Int>("tasksTotal")
+
+        if (answer) {
+            Hawk.put("tasksDone", tasksDone + 1)
+        }
+
+        Hawk.put("tasksTotal", tasksTotal + 1)
+        Log.e("tasksDone", tasksDone.toString())
+        Log.e("tasksTotal", tasksTotal.toString())
     }
 }
